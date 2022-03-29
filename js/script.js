51bc4877915530a987d291bf97fe4bde2d42bf2c
allengr220 <45504710+allengr220@users.noreply.github.com>
2022-03-29 00:38:26 -0500

    this

--- js/script.js
+++ js/script.js
@@ -118,12 +118,12 @@ function buildAndShowHomeHTML (categories) {
 
        var insertableChosenCategoryShortName = "'chosenCategoryShortName'";
        var stringLoadMenuItem = "randomCategoryShortName";
-       var thing = window.querySelectorAll("a").item(1).getAttribute("onclick");
+       var thing = this.querySelectorAll("a").item(1).getAttribute("onclick");
        console.log(thing);
           insertProperty(thing, stringLoadMenuItem, insertableChosenCategoryShortName);
 console.log(thing);
 
-       var homeHtmlToInsertIntoMainPage = window.querySelectorAll("a").item(1).setAttribute("onclick", thing);
+       var homeHtmlToInsertIntoMainPage = this.querySelectorAll("a").item(1).setAttribute("onclick", thing);
 
 
 
